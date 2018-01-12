4074d7534865223a3d7c912cbc0e700a10681f1b
Jackie <984137184@qq.com>
2018-01-12 17:40:20 +0800

    初步更新数据库工具类

---
+++ MyQQ/ConnectionUtil.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+namespace MyQQ
+{
+    /// <summary>
+    /// 数据库连接工具类,可以连接MySQL或SQLServer
+    /// </summary>
+    public class ConnectionUtil
+    {
+        private string connStr = "server=.;database=myqq;uid=sa;pwd=sa"; 
+
+        public int ExecuteNoneQuery(string sql)
+        {
+            SqlConnection connection = null;
+            int result = -1;
+            try
+            {
+                connection = new SqlConnection(connStr);
+                connection.Open();
+                SqlCommand command = new SqlCommand(sql, connection);
+                result = command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return result;
+        }
+        public static int ExecuteScalar(string sql) { }
+    }
+}
---
+++ MyQQ/Test.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyQQ
+{
+    class Test
+    {
+
+        public void test()
+        {
+            var json = DynamicJson.Serialize(new { Id = "", Name = "" });
+            var data = DynamicJson.Parse(json);
+            
+        }
+
+    }
+}
